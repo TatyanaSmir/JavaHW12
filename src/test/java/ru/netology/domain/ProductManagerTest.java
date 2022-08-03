872aef7a153d9afbb3a46fa4ea352a2f63550968
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-08-03 09:36:59 +0300

    Tests added

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -13,6 +13,7 @@ public class ProductManagerTest {
     Product product2 = new Book(2, "B", 50, "BBB");
     Product product3 = new Smartphone(3, "Samsung", 5_000, "삼성");
     Product product4 = new Smartphone(4, "Iphone", 6_000, "Apple Inc.");
+    Product product5 = new Smartphone(4, "Iphone", 6_000, "Apple Inc.");
 
 
     @BeforeEach
@@ -22,15 +23,34 @@ public class ProductManagerTest {
         manager.add(product2);
         manager.add(product3);
         manager.add(product4);
+        manager.add(product5);
     }
 
 
     @Test
-    public void shouldFindProductByName() {
+    public void shouldFindOneProductByName() {
         Product[] expected = {product1};
         Product[] actual = manager.searchBy("A");
 
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMoreThanOneProductByName() {
+        Product[] expected = {product4, product5};
+        Product[] actual = manager.searchBy("Iphone");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindNothingByName() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Синхрофазатрон");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
     }
 
 }
